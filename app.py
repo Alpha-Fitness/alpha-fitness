bd83ad9ad724087af5309a629e7720ac2e281a50
J1309 <justineapen13@gmail.com>
2026-04-13 22:39:27 +0530

    Fix logo - add send_from_directory

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file, send_from_directory
 import sqlite3
 import os
 import io
